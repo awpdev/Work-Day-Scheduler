db8e4871a7c3149e69fdec194a041a82c5a8f5f2
Andrew Park <apark999@gmail.com>
2021-02-11 15:58:00 -0800

    added script.js

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
 $( document ).ready(function() {
 
-  // Display which day of week and date
+  // Display day of week and date
   $("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMM Do'));
 
-  let workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-  // let userText = ["", "", "", "", "", "", "", "", ""];
-  let currentHour = parseInt(moment().format('kk'));
+  const workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
-  // Dynamically create a row for each work hour
-  for (let i = 0; i < workHours.length; i++) {
-    let timeSlot = $("<div>").addClass("row");
-    timeSlot.attr("id", "row-" + i);
-    let timeCol = $("<div>").addClass("col-sm-1 hour");
-    let eventCol = $("<textarea>").addClass("col-sm-10");
-    if (currentHour > i + 9) {
-      eventCol.addClass("past");
-    } else if (currentHour < i + 9) {
-      eventCol.addClass("future");
-    } else {
-      eventCol.addClass("current");
-    }
-    let btnCol = $("<div>").addClass("col-sm-1 saveBtn");
-    timeSlot.attr("time", workHours[i]);
-    timeCol.text(timeSlot.attr("time"));
-    let saveIcon = $("<i>");
-    saveIcon.addClass("far fa-save");
+  const currentHour = parseInt(moment().format('kk')) - workHours.length; // use to color code the time blocks
+  
+  renderUI();
 
-    btnCol.append(saveIcon);
-    timeSlot.append(timeCol);
-    timeSlot.append(eventCol);
-    timeSlot.append(btnCol);
+  // click event handler for save buttons
+  $(document).on('click', '.saveBtn', function(event) {
+    event.preventDefault(); 
     
-    $(".container").append(timeSlot);
+    // retrieve text from the text block on the same row
+    let hour = $(this).attr("id");
+    let note = $(this).siblings(".user-note").val();
+    // save to local storage
+    if (note) {
+      localStorage.setItem(hour, note);
+    } 
+    textSaved = true;
 
-  }
-
-  $(".saveBtn").on("click", function() {
-
-    if (typeof(Storage) !== "undefined") {
-      
-    } else {
-      alert("Sorry, your browser does not support web storage.");
-    }
+    renderUI();
   
   });
 
+  function renderUI() {
+    $(".container").empty();
+
+    // Dynamically create a row for each work hour
+    for (let i = 0; i < workHours.length; i++) {
+      let timeSlot = $("<div>").addClass("row");
+      timeSlot.attr("id", "row-" + i);
+      let timeCol = $("<div>").addClass("col-1 hour");
+      let eventCol = $("<textarea>").addClass("col-10");
+      eventCol.attr("id", "text-hr" + i);
+      eventCol.addClass("user-note");
+      if (currentHour > i) {
+        eventCol.addClass("past");
+      } else if (currentHour < i) {
+        eventCol.addClass("future");
+      } else {
+        eventCol.addClass("current");
+      }
+      let btnCol = $("<div>").addClass("col-sm-1 saveBtn");
+      btnCol.attr("id", "hr" + i);
+      timeSlot.attr("time", workHours[i]);
+      timeCol.text(timeSlot.attr("time"));
+      let saveIcon = $("<i>");
+      saveIcon.addClass("far fa-save");
+
+      btnCol.append(saveIcon);
+      timeSlot.append(timeCol);
+      timeSlot.append(eventCol);
+      timeSlot.append(btnCol);
+      
+      $(".container").append(timeSlot);
+      getNotes();
+    }
+    
+    // if (textSaved) console.log(userText);
+    console.log("rendered");
+  }
+
+  // retrieve user text from local storage
+  function getNotes() {
+    console.log("getnotes");
+    $("#text-hr0").text(localStorage.getItem("hr0"));
+    $("#text-hr1").text(localStorage.getItem("hr1"));
+    $("#text-hr2").text(localStorage.getItem("hr2"));
+    $("#text-hr3").text(localStorage.getItem("hr3"));
+    $("#text-hr4").text(localStorage.getItem("hr4"));
+    $("#text-hr5").text(localStorage.getItem("hr5"));
+    $("#text-hr6").text(localStorage.getItem("hr6"));
+    $("#text-hr7").text(localStorage.getItem("hr7"));
+    $("#text-hr8").text(localStorage.getItem("hr8"));
+  }
 });
+
